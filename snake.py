31e88212bc6a56d0e8dfe2fa64c661c2fa5c5468
Emil J <emil.jahnke@icloud.com>
2019-10-21 10:51:42 +0200

    made code nicer

--- snake.py
+++ snake.py
@@ -46,17 +46,8 @@ class Button(pygame.sprite.Sprite):
             x - self.image.get_width() / 2), int(y - self.image.get_height() / 2)
 
     def pressed(self, mouse):
-        if mouse[0] > self.rect.topleft[0]:
-            if mouse[1] > self.rect.topleft[1]:
-                if mouse[0] < self.rect.bottomright[0]:
-                    if mouse[1] < self.rect.bottomright[1]:
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                return False
+        if mouse[0] > self.rect.topleft[0] and mouse[1] > self.rect.topleft[1] and mouse[0] < self.rect.bottomright[0] and mouse[1] < self.rect.bottomright[1]:
+            return True
         else:
             return False
 
